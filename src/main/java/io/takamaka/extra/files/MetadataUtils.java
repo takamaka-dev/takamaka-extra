c9a2fe5f3bbcef7d1160c63a0bbbda9e9fb62404
zeikeneim <alessandro.pasi@takamaka.io>
2023-01-17 17:53:37 +0100

    fix SbumitBlob

---
+++ src/main/java/io/takamaka/extra/files/MetadataUtils.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.takamaka.extra.files;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.takamaka.wallet.utils.TkmTextUtils;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author tyranneo
+ */
+@Slf4j
+public class MetadataUtils {
+
+    public static final void collectMetadata(FileInputStream fileIn, String[] tags, String filename) throws IOException, SAXException, TikaException {
+        Metadata extractMetadatatUsingParser = extractMetadatatUsingParser(fileIn);
+
+        String[] names = extractMetadatatUsingParser.names();
+
+        Map<String, String> mappedMetaData = new HashMap<>();
+        Map<String, String> mappedExtraMetadata = new HashMap<>();
+
+        for (String name : names) {
+            mappedMetaData.put(name, extractMetadatatUsingParser.get(name));
+        }
+
+        ObjectMapper jacksonMapper = TkmTextUtils.getJacksonMapper();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        JsonGenerator gen = jacksonMapper.createGenerator(baos);
+
+        gen.writeStartObject();
+
+        if (tags != null && tags.length != 0) {
+
+            /*
+            Another method could be the following
+            String[] tags = "tag1,tag2,tag3".split(",");
+             */
+            gen.writeFieldName("tags");
+            gen.writeStartArray();
+            for (String tag : tags) {
+                String trimmedTag = StringUtils.trimToNull(tag);
+                if (!TkmTextUtils.isNullOrBlank(trimmedTag)) {
+                    gen.writeObject(trimmedTag);
+                }
+            }
+            gen.writeEndArray();
+        }
+
+        mappedMetaData.entrySet().forEach((single) -> {
+            if (single.getKey().equals("Content-Type")
+                    || single.getKey().equals("X-Parsed-By")) {
+                try {
+                    gen.writeStringField(single.getKey(), single.getValue());
+                } catch (IOException ex) {
+                    log.warn("Unreadable metadata", ex);
+                }
+            } else {
+                mappedExtraMetadata.put(single.getKey(), single.getValue());
+            }
+        });
+        if (!TkmTextUtils.isNullOrBlank(filename)) {
+            gen.writeStringField("resourceName", filename);
+        }
+        gen.writeStringField("mime", mappedMetaData.get("Content-Type"));
+        gen.writeObjectField("extraMetadata", mappedExtraMetadata);
+
+        //to optimize indexing leave data as last element
+//        byte[] byteFile = FileUtils.readFileToByteArray(selectedFile);
+//        String base64file = TkmSignUtils.fromByteArrayToB64URL(byteFile);
+//        gen.writeStringField("data", base64file);
+
+        gen.writeEndObject();
+        gen.flush();
+
+        String generatedMap = baos.toString(StandardCharsets.UTF_8);
+
+        gen.close();
+        baos.close();
+    }
+
+    public static final Metadata extractMetadatatUsingParser(InputStream stream)
+            throws IOException, SAXException, TikaException {
+
+        Parser parser = new AutoDetectParser();
+        ContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext context = new ParseContext();
+
+        parser.parse(stream, handler, metadata, context);
+        return metadata;
+    }
+
+}
