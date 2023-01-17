0bb80067a1d0db56f20395d2c38eb20fca4ce8d1
giovanni antino <giovanni.antino@takamaka.io>
2023-01-17 17:30:21 +0100

    first commit

---
+++ src/main/java/io/takamaka/extra/TakamakaExtra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.takamaka.extra;
+
+/**
+ *
+ * @author Giovanni Antino giovanni.antino@takamaka.io
+ */
+public class TakamakaExtra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
